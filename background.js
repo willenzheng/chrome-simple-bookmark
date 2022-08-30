cc9ebacc80c8f21f127b6673a4fa706eae3e3185
zhengweiliang <zhengweiliang@yuanxiatech.com>
2022-08-30 14:41:28 +0800

    feat: 浏览器窗口失焦时，toolbar icon 会变浅

---
+++ background.js
@@ -0,0 +1,20 @@
+// 监听窗口失去焦点，动态切换toolbar图标，以达到跟chrome原生图标一样的效果
+chrome.windows.onFocusChanged.addListener(function (windowId) {
+  chrome.tabs.query({}).then(tabs => {
+    // 使用倒序遍历，提高响应速度
+    console.log(tabs)
+    for (let i = tabs.length - 1; i >= 0; i--) {
+      if (tabs[i].windowId === windowId) {
+        chrome.action.setIcon({
+          tabId: tabs[i].id,
+          path: "img/icon_bookmark.png"
+        })
+      } else {
+        chrome.action.setIcon({
+          tabId: tabs[i].id,
+          path: "img/icon_bookmark_gray.png"
+        })
+      }
+    }
+  })
+})
