63696b42a2620582c09dbc551c9131cbd18f83be
郑维良 <327575584@qq.com>
2023-11-02 15:00:19 +0800

    更新置灰图标

--- background.js
+++ background.js
@@ -4,15 +4,15 @@ chrome.windows.onFocusChanged.addListener(function (windowId) {
     // 使用倒序遍历，提高响应速度
     for (let i = tabs.length - 1; i >= 0; i--) {
       if (tabs[i].windowId === windowId) {
-        // chrome.action.setIcon({
-        //   tabId: tabs[i].id,
-        //   path: "img/icon_bookmark.png"
-        // })
+        chrome.action.setIcon({
+          tabId: tabs[i].id,
+          path: "img/icon_bookmark.png"
+        })
       } else {
-        // chrome.action.setIcon({
-        //   tabId: tabs[i].id,
-        //   path: "img/icon_bookmark_gray.png"
-        // })
+        chrome.action.setIcon({
+          tabId: tabs[i].id,
+          path: "img/icon_bookmark_gray.png"
+        })
       }
     }
   })
