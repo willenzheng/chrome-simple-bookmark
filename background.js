90af120ab559d13c849385b6bdb73f6dd275449b
郑维良 <327575584@qq.com>
2023-12-01 08:33:37 +0800

    优化置灰图标的逻辑

--- background.js
+++ background.js
@@ -1,26 +1,23 @@
-chrome.runtime.getPlatformInfo(function (info) {
-  if (info.os === 'mac') {
-    setIconStatus()
-  }
-})
 // 监听窗口失去焦点，动态切换toolbar图标，以达到跟chrome原生图标一样的效果
-const setIconStatus = () => {
-  chrome.windows.onFocusChanged.addListener(function (windowId) {
-    chrome.tabs.query({}).then(tabs => {
-      // 使用倒序遍历，提高响应速度
-      for (let i = tabs.length - 1; i >= 0; i--) {
-        if (tabs[i].windowId === windowId) {
-          chrome.action.setIcon({
-            tabId: tabs[i].id,
-            path: "img/icon_bookmark.png"
-          })
-        } else {
-          chrome.action.setIcon({
-            tabId: tabs[i].id,
-            path: "img/icon_bookmark_gray.png"
-          })
+chrome.windows.onFocusChanged.addListener(function (windowId) {
+  chrome.runtime.getPlatformInfo(function (info) {
+    if (info.os === 'mac') {
+      chrome.tabs.query({}).then(tabs => {
+        // 使用倒序遍历，提高响应速度
+        for (let i = tabs.length - 1; i >= 0; i--) {
+          if (tabs[i].windowId === windowId) {
+            chrome.action.setIcon({
+              tabId: tabs[i].id,
+              path: "img/icon_bookmark.png"
+            })
+          } else {
+            chrome.action.setIcon({
+              tabId: tabs[i].id,
+              path: "img/icon_bookmark_gray.png"
+            })
+          }
         }
-      }
-    })
+      })
+    }
   })
-}
+})
